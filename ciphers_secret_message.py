bdd92c6c19931741eba9a91b41edd8ff1109c92a
szymonsli <szymon.sliwinski@onet.com.pl>
2025-07-31 12:54:16 +0200

    Optimized ciphers secret message solution

--- ciphers_secret_message.py
+++ ciphers_secret_message.py
@@ -1,8 +1,9 @@
 cipher = "a_up4qr_kaiaf0_bujktaz_qm_su4ux_cpbq_ETZ_rhrudm"
 
+
 def decipher(cipher):
   return "".join(
-    [chr(a) for a in range(ord("A"), ord("z") + 1) if ((a - (base := ord('A') if c.isupper() else ord('a')) + i) % 26 == ord(c) - base) and c.isupper() == chr(a).isupper() and chr(a).isalpha()][0]
+    chr((base := ord('A') if c.isupper() else ord('a')) + (ord(c) - base - i) % 26)
     if c.isalpha() else c
     for i, c in enumerate(cipher)
   )
